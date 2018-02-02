a3259f548b7546408c1e615a3d1a58fd6cf51306
Khalil Stemmler <metroidman12@gmail.com>
2018-02-01 20:41:19 -0500

    Initial comit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Single Entity Quantitative Breakdown
---
+++ src/styles.js
@@ -0,0 +1,108 @@
+
+const styles = {
+  processContainer: {
+    background: "white",
+    width: "45%",
+    margin: "6px",
+    padding: "19px",
+    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+    padding: "20px",
+    borderRadius: "3px",
+    maxWidth: "317px",
+  },
+  processesRow: {
+    display: "flex",
+    justifyContent: "flex-start",
+    marginBottom: "24px",
+  },
+  processesRowAlignLeft :{
+    display: "flex",
+    justifyContent: "baseline",
+    marginBottom: "24px",
+  },
+  processTitle: {
+    fontSize: "22px",
+    fontWeight: "200",
+  },
+  processBar: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: "11px",
+  },
+  processListBreakdown: {
+    listStyle: "none",
+    paddingLeft:"5px",
+    paddingRight:"5px"
+  },
+  circle: {
+    height: "10px",
+    width: "10px",
+    borderRadius: "25px",
+    marginRight: "11px"
+  },
+  element: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "6px",
+    justifyContent: "space-between",
+    flexWrap: "nowrap",
+  },
+  name: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: "20px",
+    position: "sticky",
+    minWidth: "159px",
+    fontSize: "14px",
+    fontWeight: "400",
+    color: "#6b6b6b",
+    fontWeight: "300",
+  },
+  totalName: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: "20px",
+    position: "sticky",
+    minWidth: "159px",
+    fontSize: "17px",
+    fontWeight: "400",
+    color: "#6b6b6b",
+    fontWeight: "300",
+  },
+  value: {
+    color: "#6b6b6b",
+    marginLeft: "20px",
+    fontWeight: "300",
+  },
+  divider: {
+    border: "solid 1px #d4d4d4",
+    width: "100%",
+  },
+  supplementalList: {
+    margin: "0",
+    padding: "0",
+    paddingLeft: "5px",
+    listStyle: "none",
+    fontSize: "14px",
+    color: "#6b6b6b",
+    background: "#f3f3f3",
+    fontWeight: "300",
+    padding: "17px",
+    borderRadius: "3px",
+  },
+  moreInfo: {
+    minWidth: "159px",
+    fontSize: "17px",
+    fontWeight: "400",
+    color: "#6b6b6b",
+    fontWeight: "300",
+    marginBottom: "6px",
+  }
+}
+
+export default styles;
+
+
